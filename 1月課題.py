1eaffde15cd30ee7d08dc45869a4656f47b724a4
imai.shunsuke@ark-info-sys.co.jp <imai.shunsuke@ark-info-sys.co.jp>
2019-02-06 18:07:04 +0900

    step3-1まで追加

--- 1月課題.py
+++ 1月課題.py
@@ -2,22 +2,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
+DICE_NUM=3
+VALUE_NUM=2
+# STEP1
 # 1,2,3番目のサイコロの含有率πの初期値
-PI_1 = 0.3
-PI_2 = 0.5
-PI_3 = 0.2
+PI = [0.3,0.5,0.2]
+PINEW = [0,0,0]
 
-# 1,2,3番目のサイコロの奇数の目が出る確率
-THETA_11 = 0.8
-THETA_21 = 0.6
-THETA_31 = 0.3
+# 1,2,3番目のサイコロの奇数、偶数の目が出る確率(既知)
+THETA = [[0.8,0.2],[0.6,0.4],[0.3,0.7]]
 
-STEP = 10000
+# コイン投げの試行回数
+N = 10000
 
 # 奇数、偶数の目が出た回数
-R1 = 4746
-R2 = 5254
+R = [4746,5254]
 
 # 収束判定用の値
-CONV = 0.001
+EPS = 1.0e-8
 
+# P(w_i|v_k)事後確率についてi=1,2,3(サイコロの種類) k=1,2(奇数or偶数)
+P_wi_vk=[[0 for i in range(2)] for j in range(3)]
+#P_wi_vk=[[0,0],[0,0],[0,0]]
+
+
+# step2 P(w_i|v_k)の更新
+for i in range(DICE_NUM):
+    for k in range(VALUE_NUM):
+        P_wi_vk[i][k] = PI[i]*THETA[i][k]
+
+
+# step3-1 π^iの更新
+for i in range(DICE_NUM):
+    for k in range(VALUE_NUM):
+        PINEW[i] = 1/N*(R[k]*P_wi_vk[i][k])
+
+
+# step3-2 更新したπ^iをベイズの定理に代入してP(w_i|v_k)の更新
+#P_wi_vk_NEW =
